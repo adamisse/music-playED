d391111ff5c0e529845fec49f4e6c24cb11067ea
adamisse <matheus.abmoreira@gmail.com>
2021-08-20 20:12:45 -0300

    Remoção da função de printar música dentro de tMusica.h. Comentários finalizados, exceto em tUsuario.h

--- include/tMusica.h
+++ include/tMusica.h
@@ -1,23 +1,45 @@
 #ifndef T_MUSICA
 #define T_MUSICA
 
+//Definição da estrutura responsável pela lógica das músicas;
 typedef struct Musica tMusica;
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-tMusica *inicializaMusica(char *banda,char *musica);
+/*  
+ *  Inicializa o struct de tMusica com as strings de *banda e *musica, respectivamente;
+ *  inputs: Dois ponteiros de char, para as strings de banda e música;
+ *  output: Um ponteiro para o tipo tMusica que foi inicializado;
+ *  pos-condição: Retorno de um ponteiro para o tipo tMusica;
+*/
+tMusica *inicializaMusica(char *banda, char *musica);
 
+/*
+ *  Função responsável por liberar o ponteiro do tipo tMusica bem como as strings de nomes da banda e música;
+ *  Inputs: Um ponteiro para o tipo tMusica;
+ *  pre-condição: Um ponteiro do tipo tMusica estar alocado;
+ *  pos-condição: O ponteiro de tMusica é liberado juntamente de suas informações;
+*/
 void liberaMusica(tMusica *musica);
 
-void printaMusica(tMusica *musica);
-
+/*
+ *  Retorna a string do nome da música, contida na estrutura tMusica;
+ *  inputs: Um ponteiro para o tipo tMusica;
+ *  output: O nome da música contido no ponteiro da struct;
+ *  pre-condição: Um ponteiro do tipo tMusica estar alocado;
+ *  pos-condição: Retorno de uma string 'nomeMus' do tipo tMusica;
+*/
 char *retornaNomeMus(tMusica *musica);
 
+/*
+ *  Retorna a string do nome da banda, contida na estrutura tMusica;
+ *  inputs: Um ponteiro para o tipo tMusica;
+ *  output: O nome da banda contido no ponteiro da struct;
+ *  pre-condição: Um ponteiro do tipo tMusica estar alocado;
+ *  pos-condição: Retorno de uma string 'nomeBand' do tipo tMusica;
+*/
 char *retornaNomeBan(tMusica *musica);
-
-
-
 
 #endif
--- include/tPlayList.h
+++ include/tPlayList.h
@@ -1,6 +1,7 @@
 #ifndef T_PLAYLIST
 #define T_PLAYLIST
 
+//Definição da estrutura responsável pela lógica das playlists;
 typedef struct Playlist tPlaylist;
 
 #include <stdio.h>
@@ -9,16 +10,53 @@ typedef struct Playlist tPlaylist;
 #include "tListaMusica.h"
 #include "tListaPlayList.h"
 
+/*  
+ *  Inicializa o struct de tPlaylist, fazendo a sua alocação e chamando uma função de inicialização para a sua sentinela;
+ *  output: Um ponteiro para o tipo tPlaylist que foi inicializado;
+ *  pos-condição: Retorno de um ponteiro para o tipo tPlaylist;
+*/
 tPlaylist *inicializaPlayList();
 
+/*
+ *  Função responsável pela abertura dos arquivos de playlists, bem como a captura de suas informações; 
+ *  Inputs: Um ponteiro do tipo tPlaylist, e uma string para armazenar o nome da playlist;
+ *  pre-condição: A existência do arquivo de interesse e a playlist em questão ter sido previamente inicializada;
+ *  pos-condição: Há o armazenamento das músicas contidas na playlist de entrada dentro de uma célula do tipo tListaMusica;
+*/
 void preenchePlayList(tPlaylist *playList, char *nome);
 
+/*
+ *  Função responsável pelo preenchimento da nova playlist, bem como a modificação do nome (que recebe o nome da banda/artista);
+ *  Inputs: Um ponteiro do tipo tPlaylist, um ponteiro do tipo tMusica e uma string para o novo nome da playlist;
+ *  pre-condição: Os ponteiros tPlaylist e tMusica previamente alocados e a entrada ser feita de maneira correta;
+ *  pos-condição: A playlist passa a ter o nome passado no parâmetro da função e é chamada uma função para inserir as músicas dentro da nova playlist;
+*/
 void preencheNovaPlayList(tPlaylist *playList, tMusica *musica, char *nome);
 
+/*
+ *  Função responsável por liberar o ponteiro do tipo tPlaylist bem como a string do nome da playlist e a sua sentinela;
+ *  Inputs: Um ponteiro para o tipo tPlaylist;
+ *  pre-condição: Um ponteiro do tipo tPlaylist estar alocado;
+ *  pos-condição: O ponteiro de tPlaylist é liberado juntamente das informações presentes na estrutura;
+*/
 void liberaPlayList(tPlaylist *playList);
 
+/*
+ *  Retorna a string do nome da playlist, contida na estrutura tPlaylist;
+ *  inputs: Um ponteiro para o tipo tPlaylist;
+ *  output: O nome da playlist contido no ponteiro da struct;
+ *  pre-condição: Um ponteiro do tipo tPlaylist estar alocado;
+ *  pos-condição: Retorno de uma string 'nome' do tipo tPlaylist;
+*/
 char *retornaNomePlay(tPlaylist *Playlist);
 
+/*
+ *  Retorna a sentinela do nome da playlist, contida na estrutura tPlaylist;
+ *  inputs: Um ponteiro para o tipo tPlaylist;
+ *  output: Uma sentinela contida em tPlaylist;
+ *  pre-condição: Um ponteiro do tipo tPlaylist estar alocado;
+ *  pos-condição: É feito o retorno de uma sentinela 'sentMus' do tipo tPlaylist;
+*/
 tListaMusica *retornaListaMusica(tPlaylist *playList);
 
 #endif
